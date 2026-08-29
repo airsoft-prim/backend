06a886f0c6c176e6cbc870dba5a4903f8a1a310c
Oidaho <76991612+Oidaho@users.noreply.github.com>
2026-08-29 17:57:31 +1000

    feat: Add public API scheme fields description

--- backend/general/schemas.py
+++ backend/general/schemas.py
@@ -12,12 +12,12 @@ from .types import FilterMapping, SortMapping
 class PageOf[D: BaseModel](BaseModel):
     """Страница результатов с пагинацией."""
 
-    items: list[D] = Field(default_factory=list)
-    page: int = Field(ge=1, default=1)
-    total: int = Field(ge=0, default=0)
-    page_size: int = Field(default=1)
+    items: list[D] = Field(default_factory=list, description="Элементы страницы")
+    page: int = Field(ge=1, default=1, description="Номер страницы")
+    page_size: int = Field(default=1, description="Размер страницы страницы")
+    total: int = Field(ge=0, default=0, description="Всего элементов")
 
-    @computed_field
+    @computed_field(description="Всего страниц")
     @property
     def total_pages(self) -> int:
         """Вычисление общего кол-ва страниц."""
@@ -30,45 +30,45 @@ class PageOf[D: BaseModel](BaseModel):
 class SearchParams(BaseModel):
     """Параметры запроса для пагинированного поиска с фильтрацией."""
 
-    page: int = Field(ge=1, default=100)
-    page_size: int = Field(ge=1, le=100, default=30)
+    page: int = Field(ge=1, default=100, description="Номер страницы")
+    page_size: int = Field(ge=1, le=100, default=30, description="Размер страницы")
 
 
 class Filter[F: StrEnum](BaseModel):
     """Фильтрация по заданным полям."""
 
-    field: F
-    value: Any
-    operator: FilterOperator
+    field: F = Field(description="Фильтруемое поле")
+    value: Any = Field(description="Сравнительное значение")
+    operator: FilterOperator = Field(description="Оператор сравнения")
 
     @field_serializer("field", when_used="always")
     def field_as_str(self, field: F) -> str:
-        """_summary_"""
+        """Представляет поле всегда как строку при любой сериализации."""
         return field.value
 
     def to_mapping(self) -> FilterMapping:
-        """_summary_"""
+        """Приводит фильтры в независимый формат, удобный для бизнес-логики."""
         return FilterMapping(self.model_dump())
 
 
 class Sort[S: StrEnum](BaseModel):
     """Сортировка по заданным полям."""
 
-    field: S
-    direction: SortDirection = Field(default=SortDirection.ASC)
+    field: S = Field(description="Сортируемое поле")
+    direction: SortDirection = Field(default=SortDirection.ASC, description="Направление")
 
     @field_serializer("field", when_used="always")
     def field_as_str(self, field: S) -> str:
-        """_summary_"""
+        """Представляет поле всегда как строку при любой сериализации."""
         return field.value
 
     def to_mapping(self) -> SortMapping:
-        """_summary_"""
+        """Приводит соритровки в независимый формат, удобный для бизнес-логики."""
         return SortMapping(self.model_dump())
 
 
 class SearchBody[F: StrEnum, S: StrEnum](BaseModel):
     """Параметры тела запроса  для пагинированного поиска с фильтрацией."""
 
-    filers: Sequence[Filter[F]]
-    sorts: Sequence[Sort[S]]
+    filers: Sequence[Filter[F]] = Field(description="Список фильтров по полям ресурса")
+    sorts: Sequence[Sort[S]] = Field(description="Список сортировок по полям ресурса")
